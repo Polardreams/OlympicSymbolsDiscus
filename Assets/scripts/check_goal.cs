a4c2f2a2080d6d1343f3335e3be65fb9570ab77d
Polardreams <polardreams.hernandez@gmail.com>
2019-12-11 23:59:11 +0100

    screenResult gravity win&lose animation and control
    set toogle to Canvas
    create physic without gravity
    lose factor is when >3x respan
    add animation to symbol (bue and yellow)
    discuss bigger
    check_freez fix

--- Assets/scripts/check_goal.cs
+++ Assets/scripts/check_goal.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,8 +11,8 @@ public class check_goal : MonoBehaviour
     private int index_hit, index_goal;
     public Text txt;
     private bool goal_flag;
-    private int max_hits;
     static public bool ScreenResult;
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +30,6 @@ public class check_goal : MonoBehaviour
         goal_tag[1] = "yellow";
         index_goal = 0;
         goal_flag = false;
-        max_hits = 5;
         index_hit = 0;
     }
 
@@ -41,14 +41,18 @@ public class check_goal : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (index_hit< max_hits) {
-            if (index_goal<=goal_tag.Length-1) {
-                if (collision.tag == goal_tag[index_goal])
+        if (discus_physic.respawn_index<4) {//Diskus hat nicht 3x respant
+            if (index_goal<=goal_tag.Length-1) {//der IST-Erreicht-Index ist kleiner als der SOLL-Index, 
+                if (collision.tag == goal_tag[index_goal])//Kollision mit dem aktuellen GameObject (bei tag)
                 {
                     txt.text = collision.tag;
-                    index_goal++;
-                    if (index_goal == goal_tag.Length)
+                    index_goal++;//nur wenn das aktuelle Symbol getroffen wird, erhöht sich der Index und wählt das neue Ziel aus
+
+                    StartCoroutine(destroySymbol(collision));
+
+                    if (index_goal == goal_tag.Length)//IST-Erreicht-Index = SOLL-INdex, die anzahl der möglichen Ziele wurde getroffen
                     {
+                        
                         goal_flag = true;
                         txt.text = "You Win";
                         ScreenResult = goal_flag;
@@ -57,17 +61,25 @@ public class check_goal : MonoBehaviour
                 }
             }
             index_hit++;
-        } else
+        } else//3x respawnt alle Versuche gescheitert, verloren
         {
             //goal_flag = false;
             goal_flag = false;
             txt.text = "You Loose";
                 ScreenResult = goal_flag;
                 SceneManager.LoadScene("resultScreen", LoadSceneMode.Single);
+            discus_physic.respawn_index = 0;
         }
         
     }
 
+    private IEnumerator destroySymbol (Collider2D collision)
+    {
+        yield return new WaitForSeconds(1f);
+        collision.GetComponent<Animator>().SetBool("ishit", true);
+        collision.GetComponent<Collider2D>().enabled = false;
+        
+    }
 
 
 }
--- Assets/scripts/discus_physic.cs
+++ Assets/scripts/discus_physic.cs
@@ -12,14 +12,27 @@ public class discus_physic : MonoBehaviour
     public int max_crosshair_radius;
     private Vector2 discus_startPosition;
 
-
+    public GameObject r1, r2, r3, r4;
+    public PhysicsMaterial2D symbol_hit;
+    public bool gravity, wasMove;
 
     private Vector2 pos_start, pos_end, pos_mov, force;
+
+    public static int respawn_index = 0;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         discus_startPosition = gameObject.transform.position;
+        wasMove = false;
+        respawn_index = 0;
+    }
+
+    public void setGravity(bool flag)
+    {
+        gravity = flag;
     }
 
     // Update is called once per frame
@@ -28,58 +41,114 @@ public class discus_physic : MonoBehaviour
         check_for_touchEvent();
         cam.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -13);
         check_freez();
-        discus_info.text = "Velocity.Y: " + gameObject.GetComponent<Rigidbody2D>().velocity.y.ToString()+'\n';
-        discus_info.text = discus_info.text + "Position: " + gameObject.transform.position.ToString();
+        check_move();
+        discus_info.text = "Velocity.Y: " + gameObject.GetComponent<Rigidbody2D>().velocity.y.ToString() + '\n';
+        discus_info.text = discus_info.text + "Position: " + gameObject.transform.position.ToString()+ '\n' + respawn_index;
     }
 
-    private void check_freez ()
+    private void check_freez()
     {
-        if (gameObject.GetComponent<Rigidbody2D>().velocity.y == 0)
+        double speed_x = Math.Round(gameObject.GetComponent<Rigidbody2D>().velocity.x, 2);
+        double speed_y = Math.Round(gameObject.GetComponent<Rigidbody2D>().velocity.y, 2);
+
+        if (gravity)
         {
-            gameObject.transform.position = discus_startPosition;
+            if (speed_y == 0 && speed_x == 0 && wasMove == true)
+            {
+                gameObject.transform.position = discus_startPosition;
+                gameObject.transform.rotation = Quaternion.Euler(0,0,0);
+                respawn_index++;
+                wasMove = false;
+            }
         }
+        else
+        {
+            if (speed_y == 0 && speed_x == 0 && wasMove == true)
+            {
+                gameObject.transform.position = discus_startPosition;
+                respawn_index++;
+                wasMove = false;
+            }
+        }
+
     }
 
-    private void check_move ()
+    private void check_move()
     {
-        if (gameObject.GetComponent<Rigidbody2D>().velocity.y !=0)
+        double speed_x = Math.Round(gameObject.GetComponent<Rigidbody2D>().velocity.x, 2);
+        double speed_y = Math.Round(gameObject.GetComponent<Rigidbody2D>().velocity.y, 2);
+
+        if (speed_x > 0 || speed_y > 0)
         {
-            //...
+            wasMove = true;
         }
     }
 
     private void check_for_touchEvent()
     {
-        if (Input.touches.Length == 1)
+        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
         {
-
-            if (Input.touches[0].phase == TouchPhase.Began)
+            if (Input.touches.Length == 1)
             {
-                pos_start = new Vector2(Input.touches[0].position.x, Input.touches[0].position.y);
+
+                if (Input.touches[0].phase == TouchPhase.Began)
+                {
+                    pos_start = new Vector2(Input.touches[0].position.x, Input.touches[0].position.y);
+                    crosshair.GetComponent<SpriteRenderer>().enabled = true;
+                }
+                else
+                {
+                    if (Input.touches[0].phase == TouchPhase.Ended)
+                    {
+                        pos_end = new Vector2(Input.touches[0].position.x, Input.touches[0].position.y);
+                        force = new Vector2(pos_start.x - pos_end.x, pos_start.y - pos_end.y);
+                        discus_shoot(force);
+                        crosshair.GetComponent<SpriteRenderer>().enabled = false;
+                    }
+                    else
+                    {
+                        if (Input.touches[0].phase == TouchPhase.Moved)
+                        {
+                            pos_mov = new Vector2(Input.touches[0].position.x, Input.touches[0].position.y);
+                            force = new Vector2(pos_start.x - pos_mov.x, pos_start.y - pos_mov.y);
+                            move_crosshair(force);
+                        }
+                    }
+                }
+            }
+        } else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                pos_start = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
                 crosshair.GetComponent<SpriteRenderer>().enabled = true;
             }
             else
             {
-                if (Input.touches[0].phase == TouchPhase.Ended)
+                if (Input.GetMouseButtonUp(0))
                 {
-                    pos_end = new Vector2(Input.touches[0].position.x, Input.touches[0].position.y);
-                    force = new Vector2(pos_start.x-pos_end.x, pos_start.y-pos_end.y);
+                    pos_end = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                    force = new Vector2(pos_start.x - pos_end.x, pos_start.y - pos_end.y);
                     discus_shoot(force);
                     crosshair.GetComponent<SpriteRenderer>().enabled = false;
-                } else
+                }
+                else
                 {
-                    if (Input.touches[0].phase == TouchPhase.Moved)
+                    if (Input.GetMouseButton(0))
                     {
-                        pos_mov = new Vector2(Input.touches[0].position.x, Input.touches[0].position.y);
+                        pos_mov = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
                         force = new Vector2(pos_start.x - pos_mov.x, pos_start.y - pos_mov.y);
                         move_crosshair(force);
                     }
                 }
             }
+
         }
+
+
     }
 
-    private void move_crosshair (Vector2 v)
+    private void move_crosshair(Vector2 v)
     {
         crosshair.transform.position = get_crosshair_position(v);
     }
@@ -90,34 +159,54 @@ public class discus_physic : MonoBehaviour
         float alpha = getAlpha(v);//Festlegung, jeder Swipe nach unten erhöht den Grad
         int factor = 100;
         float power = get_ScalfaktorToForce(v, factor).x;
-        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2 (power, (alpha*(factor/4))));
 
-        debug_text.text = "Angle: " +alpha+ '\n';
+        if (gravity)
+        {
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+            r1.GetComponent<Collider2D>().sharedMaterial = null;
+            r2.GetComponent<Collider2D>().sharedMaterial = null;
+            r3.GetComponent<Collider2D>().sharedMaterial = null;
+            r4.GetComponent<Collider2D>().sharedMaterial = null;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(power, (alpha * (factor / 4))));
+        }
+        else
+        {
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+            r1.GetComponent<Collider2D>().sharedMaterial = symbol_hit;
+            r2.GetComponent<Collider2D>().sharedMaterial = symbol_hit;
+            r3.GetComponent<Collider2D>().sharedMaterial = symbol_hit;
+            r4.GetComponent<Collider2D>().sharedMaterial = symbol_hit;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(power, (alpha * (factor / 4))));
+        }
+
+        debug_text.text = "Angle: " + alpha + '\n';
         debug_text.text = debug_text.text + "Power: " + power;
     }
 
-    private Vector2 get_crosshair_position (Vector2 v)
+    private Vector2 get_crosshair_position(Vector2 v)
     {
-        Vector2 pos = new Vector2 (0,0);
+        Vector2 pos = new Vector2(0, 0);
         float alpha = getAlpha(v);//Festlegung, jeder Swipe nach unten erhöht den Grad
-        
+
         float r = 0;
         if (get_ScalfaktorToForce(v, 1).x < max_crosshair_radius)
         {
             r = get_ScalfaktorToForce(v, 1).x;
-        } else
+        }
+        else
         {
             r = max_crosshair_radius;
-        }       
-        
-        float x = Mathf.Cos((float) DegreeToRadian((double) alpha)) * r;//cos funktion prüfen
-        float y =0;
-        if (v.x>0)
+        }
+
+        float x = Mathf.Cos((float)DegreeToRadian((double)alpha)) * r;//cos funktion prüfen
+        float y = 0;
+        if (v.x > 0)
         {
             y = Mathf.Sin((float)DegreeToRadian((double)alpha)) * r;
-        } else
+        }
+        else
         {
-            y = Mathf.Sin((float)DegreeToRadian((double)alpha)) * r*-1;
+            y = Mathf.Sin((float)DegreeToRadian((double)alpha)) * r * -1;
         }
 
         pos = new Vector2(gameObject.transform.position.x + x, gameObject.transform.position.y + y);
@@ -145,7 +234,7 @@ public class discus_physic : MonoBehaviour
          * https://docs.unity3d.com/Manual/UnderstandingFrustum.html
          **/
 
-        Vector2 scale = new Vector2(0,0);//ini
+        Vector2 scale = new Vector2(0, 0);//ini
         try
         {
             float distance = cam.orthographicSize * 2;//Camera's half-size when in orthographic mode.
@@ -157,14 +246,15 @@ public class discus_physic : MonoBehaviour
 
             //scale = new Vector2((100/ ScreenWidth * v.x)*(frustumWidth / 100) *factor, (100/ ScreenHeight * v.y)*(frustumHeight/100)*factor);
             scale = new Vector2((100 / ScreenWidth * v.x) * (frustumWidth / 100) * factor, 0); //Höhe wird von Alpha bestimmt
-        } catch (Exception e)
+        }
+        catch (Exception e)
         {
             debug_text.text = e.Message;
         }
         return scale;
     }
 
-    private float ScreenToFrustum (float px)
+    private float ScreenToFrustum(float px)
     {
         float distance = cam.orthographicSize * 2;//Camera's half-size when in orthographic mode.
         float frustumHeight = 2.0f * distance * Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
@@ -178,26 +268,27 @@ public class discus_physic : MonoBehaviour
         return result;
     }
 
-    private float getAlpha (Vector2 v)
+    private float getAlpha(Vector2 v)
     {
-        
+
         float alpha = (360 / (Screen.width * 0.5f)) * v.y;
-        
-        
+
+
         if (v.x > 0)
         {
-            if (v.y>0)
+            if (v.y > 0)
             {
-                if (alpha>90)
+                if (alpha > 90)
                 {
                     alpha = 90;
                 }
-            } else
+            }
+            else
             {
-                if (alpha<-90)
+                if (alpha < -90)
                 {
                     alpha = -90;
-                    
+
                 }
             }
         }

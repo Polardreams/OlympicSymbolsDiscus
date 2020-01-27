2812a0849627c19237998cc05fd9c0a5cef112da
lang5tuo <1498147960@qq.com>
2020-01-27 21:13:37 +0800

    moving trail for discus added

---
+++ Assets/ImportAssets yifan assets/DiscusTrail.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscusTrail : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 Pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.position = Pos;
+        Pos.z = 0;
+        if (Input.GetMouseButton(0))
+        {
+            transform.position = Pos;
+        }
+    }
+}

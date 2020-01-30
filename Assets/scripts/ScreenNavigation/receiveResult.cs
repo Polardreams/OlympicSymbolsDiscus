f8ec7d85468dc810d2a3fce90aa1175db8953dfe
Polardreams <polardreams.hernandez@gmail.com>
2020-01-30 17:08:59 +0100

    Sound and Animation and Screen
    Sound files implement
    Medal Screen Videos implement with
    Rednertexture and Material displayed on a screen behind Canvas

--- Assets/scripts/ScreenNavigation/receiveResult.cs
+++ Assets/scripts/ScreenNavigation/receiveResult.cs
@@ -10,18 +10,25 @@ public class receiveResult : MonoBehaviour
     public GameObject silver;
     public GameObject bronxe;
 
+    public GameObject gold_vid;
+    public GameObject silver_vid;
+    public GameObject bronxe_vid;
+
+    public GameObject Linewall;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
         switch (check_goal.medal_index)
         {
-            case 0: bronxe.GetComponent<Canvas>().enabled = true; GameObject.Find("txt_scoreB").GetComponent<Text>().text = "Score: " + PlayerPrefs.GetInt("score").ToString(); save_medal(check_goal.medal_index); break;
-            case 1: silver.GetComponent<Canvas>().enabled = true; PlayerPrefs.SetInt("unlockStages", check_goal.index + 1); GameObject.Find("txt_scoreS").GetComponent<Text>().text = "Score: " + PlayerPrefs.GetInt("score").ToString(); save_medal(check_goal.medal_index); break;
-            case 2: gold.GetComponent<Canvas>().enabled = true; PlayerPrefs.SetInt("unlockStages", check_goal.index + 1); GameObject.Find("txt_scoreG").GetComponent<Text>().text = "Score: " + PlayerPrefs.GetInt("score").ToString(); save_medal(check_goal.medal_index); break;
+            case 0: bronxe.GetComponent<Canvas>().enabled = true; GameObject.Find("txt_scoreB").GetComponent<Text>().text = "Score: " + PlayerPrefs.GetInt("score").ToString(); save_medal(check_goal.medal_index); bronxe_vid.GetComponent<UnityEngine.Video.VideoPlayer>().Play(); Linewall.GetComponent<MeshRenderer>().enabled = true; break;
+            case 1: silver.GetComponent<Canvas>().enabled = true; PlayerPrefs.SetInt("unlockStages", check_goal.index + 1); GameObject.Find("txt_scoreS").GetComponent<Text>().text = "Score: " + PlayerPrefs.GetInt("score").ToString(); save_medal(check_goal.medal_index); silver_vid.GetComponent<UnityEngine.Video.VideoPlayer>().Play(); Linewall.GetComponent<MeshRenderer>().enabled = true; break;
+            case 2: gold.GetComponent<Canvas>().enabled = true; PlayerPrefs.SetInt("unlockStages", check_goal.index + 1); GameObject.Find("txt_scoreG").GetComponent<Text>().text = "Score: " + PlayerPrefs.GetInt("score").ToString(); save_medal(check_goal.medal_index); gold_vid.GetComponent<UnityEngine.Video.VideoPlayer>().Play(); Linewall.GetComponent<MeshRenderer>().enabled = true; break;
         }
     }
+
+
 
     private void save_medal(int medalIndex)
     {
--- Assets/scripts/physic/discus_collision.cs
+++ Assets/scripts/physic/discus_collision.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class discus_collision : MonoBehaviour
 {
     public static Collider2D discusCollision;
-    public AudioSource hit, bounce;
+    public AudioSource hit1, hit2, hit3, hit4, hit5, bounce;
     
 
     // Start is called before the first frame update
@@ -33,11 +33,11 @@ public class discus_collision : MonoBehaviour
         switch (collision.tag)
         {
             case "ground": bounce.PlayOneShot(bounce.clip); break;
-            case "green": hit.PlayOneShot(hit.clip); break;
-            case "blue": hit.PlayOneShot(hit.clip); break;
-            case "yellow": hit.PlayOneShot(hit.clip); break;
-            case "black": hit.PlayOneShot(hit.clip); break;
-            case "red": hit.PlayOneShot(hit.clip); break;
+            case "green": hit1.PlayOneShot(hit1.clip); break;
+            case "blue": hit2.PlayOneShot(hit2.clip); break;
+            case "yellow": hit3.PlayOneShot(hit3.clip); break;
+            case "black": hit4.PlayOneShot(hit4.clip); break;
+            case "red": hit5.PlayOneShot(hit5.clip); break;
         }
         
     }
